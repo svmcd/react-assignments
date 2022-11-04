8eec503711724df8bc076685b24bd0d28ef0ca9d
svmcd <spolat0750@gmail.com>
2022-11-04 14:33:20 +0100

    dashboard nu responsive & begonnen met myskills odpracht

---
+++ myskills/src/app/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Question from "../question/question"
+
+function App() {
+  return (
+      <Question />
+  );
+}
+
+export default App;
---
+++ myskills/src/question/question.jsx
@@ -0,0 +1,16 @@
+import "./question.css"
+
+const Question = () => {
+    return (
+        <article className="question">
+            <header className="question__header">
+                <h2>foo</h2>   
+            </header>
+            <section className="question__section">
+                ⭐⭐⭐⭐⭐
+            </section>
+        </article>
+    )
+}
+
+export default Question;
